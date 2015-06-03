3ce12fe0cbcd3ed1810edf364bb4e4c46239bd55
TheoPavlich <theodorepavlich@gmail.com>
2015-06-03 15:18:04 -0400

    Completed Loops Project

---
+++ Warmups.Loops.BLL/Warmups.Loops.BLL/LoopWarmups.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups.Loops.BLL
+{
+    public class LoopWarmups
+    {
+        public string StringTimes(string str, int n)
+        {
+            string result = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                result += str;
+            }
+            return result;
+        }
+    }
+}
