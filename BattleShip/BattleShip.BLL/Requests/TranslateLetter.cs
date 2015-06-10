093ffe504e326b52a624ca8c60bc874661fafcb0
TheoPavlich <theodorepavlich@gmail.com>
2015-06-10 10:40:27 -0400

    Letter to number library

---
+++ BattleShip/BattleShip.BLL/Requests/TranslateLetter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.BLL.Requests
+{
+    public class TranslateLetter
+    {
+
+        public Dictionary<string, int> convertLetters = new Dictionary<string, int>()
+        {
+            {"A", 1},
+            {"B", 2},
+            {"C", 3},
+            {"D", 4},
+            {"E", 5},
+            {"F", 6},
+            {"G", 7},
+            {"H", 8},
+            {"I", 9},
+            {"J", 10}
+        };
+    }
+
+}
