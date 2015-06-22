4940fa3c4506d233a14cf7b0c60d1e7cd903702a
TheoPavlich <theodorepavlich@gmail.com>
2015-06-22 13:28:10 -0400

    Flooring Basic Structure set

---
+++ FlooringProgram/FlooringUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlooringUI.Workflows;
+
+namespace FlooringUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu order = new MainMenu();
+            order.Execute();
+        }
+    }
+}
